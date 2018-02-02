e32d151985cfbe289b11c1bea16c257a922d1a1b
Looney_gongsidiannao <250384247@qq.com>
2018-02-02 15:46:52 +0800

    llll

--- app/src/main/java/net/suntrans/appmanager/BasedActivity.kt
+++ app/src/main/java/net/suntrans/appmanager/BasedActivity.kt
@@ -1,5 +1,7 @@
 package net.suntrans.appmanager
 
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -29,6 +31,11 @@ import net.suntrans.appmanager.utils.StatusBarCompat
             if (statusBarFix != null)
                 statusBarFix!!.layoutParams.height = statusBarHeight
         }
+    }
+
+
+    fun navigate(activity: Context, toActivity: Class<*>) {
+        activity.startActivity(Intent(activity, toActivity))
     }
 
 }
